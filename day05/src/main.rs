3699db669caaad7b29a09aef6eb67b9b7ca8373a
Eugene Korbut <me@mvl.ru>
2019-12-06 01:05:41 +0300

    Working processor

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,8 +1,44 @@
+#[derive(Debug, PartialEq)]
+enum Mode {
+    Position,
+    Immediate,
+}
+
+impl Mode {
+    fn from_i32(input: i32) -> Self {
+        match input {
+            0 => Mode::Position,
+            1 => Mode::Immediate,
+            n => panic!("invalid mode: {}", n),
+        }
+    }
+}
+
 #[derive(Debug)]
 enum Command {
     Halt,
-    Input { addr: usize },
-    Output { addr: usize },
+    Input {
+        addr: usize,
+    },
+    Output {
+        addr: usize,
+    },
+    Add {
+        a: i32,
+        b: i32,
+        c: i32,
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+    },
+    Mul {
+        a: i32,
+        b: i32,
+        c: i32,
+        mode_a: Mode,
+        mode_b: Mode,
+        mode_c: Mode,
+    },
 }
 
 impl Command {
@@ -11,6 +47,8 @@ impl Command {
             Self::Halt => 1,
             Self::Input { .. } => 2,
             Self::Output { .. } => 2,
+            Self::Add { .. } => 4,
+            Self::Mul { .. } => 4,
         }
     }
 
@@ -48,7 +86,6 @@ impl CPU {
     fn tick(&mut self) -> State {
         let command = decode(&self.mem[self.ip..]);
         self.process(&command);
-
         self.ip += command.size();
 
         if command.is_halt() {
@@ -77,12 +114,83 @@ impl CPU {
                 let value = self.mem[*addr];
                 self.output.push(value);
             }
+            Command::Add {
+                a,
+                b,
+                c,
+                mode_a,
+                mode_b,
+                ..
+            } => {
+                let a = match mode_a {
+                    Mode::Immediate => *a,
+                    Mode::Position => self.mem[*a as usize],
+                };
+
+                let b = match mode_b {
+                    Mode::Immediate => *b,
+                    Mode::Position => self.mem[*b as usize],
+                };
+
+                self.mem[*c as usize] = a + b;
+            }
+            Command::Mul {
+                a,
+                b,
+                c,
+                mode_a,
+                mode_b,
+                ..
+            } => {
+                let a = match mode_a {
+                    Mode::Immediate => *a,
+                    Mode::Position => self.mem[*a as usize],
+                };
+
+                let b = match mode_b {
+                    Mode::Immediate => *b,
+                    Mode::Position => self.mem[*b as usize],
+                };
+
+                self.mem[*c as usize] = a * b;
+            }
         }
     }
 }
 
+fn decode_opcode(input: i32) -> (i32, Mode, Mode, Mode) {
+    let opcode = input % 100;
+    let c = (input / 10_000) % 10;
+    let b = (input / 1_000) % 10;
+    let a = (input / 100) % 10;
+
+    (
+        opcode,
+        Mode::from_i32(a),
+        Mode::from_i32(b),
+        Mode::from_i32(c),
+    )
+}
+
 fn decode(mem: &[i32]) -> Command {
-    match mem[0] {
+    let (opcode, mode_a, mode_b, mode_c) = decode_opcode(mem[0]);
+    match opcode {
+        1 => Command::Add {
+            mode_a,
+            mode_b,
+            mode_c,
+            a: mem[1],
+            b: mem[2],
+            c: mem[3],
+        },
+        2 => Command::Mul {
+            mode_a,
+            mode_b,
+            mode_c,
+            a: mem[1],
+            b: mem[2],
+            c: mem[3],
+        },
         3 => Command::Input {
             addr: mem[1] as usize,
         },
@@ -140,5 +248,23 @@ mod test {
         cpu.run();
 
         assert_eq!(vec![99], cpu.output);
+    }
+
+    #[test]
+    fn test_decode_opcode() {
+        let (opcode, a, b, c) = decode_opcode(10102);
+        assert_eq!(2, opcode);
+        assert_eq!(Mode::Immediate, a);
+        assert_eq!(Mode::Position, b);
+        assert_eq!(Mode::Immediate, c);
+    }
+
+    #[test]
+    fn test_add() {
+        let programm = vec![1101, 11, 22, 0, 101, -30, 0, 1, 99];
+        let mut cpu = CPU::new(programm, vec![]);
+        cpu.run();
+
+        assert_eq!(cpu.mem[1], 3);
     }
 }
