c3472bd443d76d370be12fa4afe6aa8f979909ba
Eugene Korbut <me@mvl.ru>
2019-12-08 00:42:14 +0300

    First variant

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,26 +1,93 @@
+use std::collections::HashMap;
+use std::fs;
+
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[derive(Debug, PartialEq)]
 struct OrbitInfo {
-    from: String,
-    to: String,
+    primary: String,
+    secondary: String,
 }
 
 impl OrbitInfo {
     pub fn parse(input: &str) -> Result<OrbitInfo> {
-        Err("foo".into())
+        let mut parts = input.trim_matches('\n').split(')');
+        let primary = parts.next().ok_or("invalid input")?;
+        let secondary = parts.next().ok_or("invalid input")?;
+
+        Ok(OrbitInfo::new(primary, secondary))
     }
 
-    pub fn new(from: &str, to: &str) -> Self {
+    pub fn new(primary: &str, secondary: &str) -> Self {
         Self {
-            from: from.into(),
-            to: to.into(),
+            primary: primary.into(),
+            secondary: secondary.into(),
         }
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct System {
+    orbits: HashMap<String, String>,
+    distance: HashMap<String, i32>,
+}
+
+impl System {
+    fn new() -> Self {
+        Self {
+            orbits: HashMap::new(),
+            distance: HashMap::new(),
+        }
+    }
+
+    fn add(&mut self, info: OrbitInfo) {
+        let OrbitInfo { primary, secondary } = info;
+        self.orbits.insert(primary, secondary);
+    }
+
+    fn update(&mut self, key: String) -> i32 {
+        if let Some(d) = self.distance.get(&key) {
+            return *d;
+        }
+
+        let master = self.orbits.get(&key).map(|k| k.clone());
+
+        if let Some(master) = master {
+            let distance = self.update(master.clone()) + 1;
+            println!("Set distance {} to {}", master, distance);
+            self.distance.insert(key, distance);
+            distance
+        } else {
+            1
+        }
+    }
+
+    fn update_all(&mut self) {
+        let keys = self.orbits.keys().map(|k| k.clone()).collect::<Vec<_>>();
+        for k in keys {
+            self.update(k);
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    let raw = fs::read_to_string("input.txt")?;
+    let mut list = raw
+        .lines()
+        .map(|line| OrbitInfo::parse(line))
+        .collect::<Result<Vec<_>>>()?;
+
+    let mut system = System::new();
+
+    list.drain(0..).for_each(|i| {
+        system.add(i);
+    });
+
+    system.update_all();
+    let total: i32 = system.distance.values().sum();
+    dbg!(total);
+
+    Ok(())
 }
 
 #[cfg(test)]
