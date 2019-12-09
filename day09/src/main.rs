9e7ffe7feb2aa4883ce0d98827012eb3e5bd5090
Eugene Korbut <me@mvl.ru>
2019-12-09 12:03:14 +0300

    Time tracking

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,7 +1,8 @@
-mod vm;
-
 use std::fs;
+use std::time::Instant;
 use vm::{CPUInfo, CPU};
+
+mod vm;
 
 fn result(tag: &str, result: i64, info: CPUInfo) {
     println!(
@@ -11,6 +12,8 @@ fn result(tag: &str, result: i64, info: CPUInfo) {
 }
 
 fn main() {
+    let now = Instant::now();
+
     let raw = fs::read_to_string("input.txt").expect("cant read");
     let mut cpu = CPU::new_from_str(&raw, vec![1]);
     cpu.run();
@@ -20,6 +23,9 @@ fn main() {
     cpu.run();
     let task_b = (cpu.output[0], cpu.info());
 
+    let total_time = now.elapsed();
+
     result("I ", task_a.0, task_a.1);
     result("II", task_b.0, task_b.1);
+    println!("Total time: {}μs", total_time.as_micros());
 }
