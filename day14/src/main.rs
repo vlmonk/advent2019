bfb46be5e2a8d349e865d4e0eea94ff6ecbd2af3
Eugene Korbut <me@mvl.ru>
2019-12-17 13:16:50 +0300

    Parsing update

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,48 +1,112 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashMap;
+use std::fs;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[derive(Debug, PartialEq)]
 struct Reaction {
-    from: String,
+    input: Vec<(usize, String)>,
     to: String,
-    from_count: usize,
     to_count: usize,
 }
 
 impl Reaction {
-    pub fn new<T>(from_count: usize, from: T, to_count: usize, to: T) -> Self
-    where
-        T: Into<String>,
-    {
-        Self {
-            from_count,
-            to_count,
-            from: from.into(),
-            to: to.into(),
+    pub fn parse(src: &str) -> Result<Self> {
+        lazy_static! {
+            // static ref RE: Regex = Regex::new(r"(\d+) ([A-Z]+) => (\d+) ([A-Z]+)").unwrap();
+            static ref RE: Regex = Regex::new(r"(.*) => (.*)").unwrap();
         }
+        let caps = RE.captures(src).ok_or("input error")?;
+        let input_str = caps.get(1).ok_or("input error").map(|r| r.as_str())?;
+        let result_str = caps.get(2).ok_or("input error").map(|r| r.as_str())?;
+
+        dbg!(input_str);
+        dbg!(result_str);
+
+        let input = Reaction::parse_input(input_str)?;
+        let result = Reaction::parse_part(result_str)?;
+
+        Ok(Self {
+            input,
+            to: result.1,
+            to_count: result.0,
+        })
     }
 
-    pub fn parse(input: &str) -> Result<Self> {
+    fn parse_input(src: &str) -> Result<Vec<(usize, String)>> {
+        src.split(',')
+            .map(|part| Reaction::parse_part(part))
+            .collect()
+    }
+
+    fn parse_part(src: &str) -> Result<(usize, String)> {
         lazy_static! {
-            static ref RE: Regex = Regex::new(r"(\d+) ([A-Z]+) => (\d+) ([A-Z]+)").unwrap();
+            static ref RE: Regex = Regex::new(r"(\d+) ([A-Z]+)").unwrap();
         }
+        let caps = RE.captures(src).ok_or("input error")?;
+        let count = caps
+            .get(1)
+            .and_then(|input| input.as_str().parse::<usize>().ok())
+            .ok_or("input error")?;
 
-        let caps = RE.captures(input).ok_or("input error")?;
-        let parts = (1..5)
-            .map(|i| caps.get(i).map(|p| p.as_str()).ok_or("input_error".into()))
-            .collect::<Result<Vec<_>>>()?;
+        let name = caps
+            .get(2)
+            .map(|input| input.as_str().to_owned())
+            .ok_or("input error")?;
 
-        let from_count = parts[0].parse::<usize>()?;
-        let to_count = parts[2].parse::<usize>()?;
-
-        Ok(Self::new(from_count, parts[1], to_count, parts[3]))
+        Ok((count, name))
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug)]
+struct ReactionInfo {
+    reaction: Reaction,
+    weight: Option<usize>,
+}
+
+impl ReactionInfo {
+    pub fn new(reaction: Reaction) -> Self {
+        Self {
+            reaction: reaction,
+            weight: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Lab {
+    input: HashMap<String, ReactionInfo>,
+}
+
+impl Lab {
+    pub fn parse(src: &str) -> Result<Self> {
+        let mut input = HashMap::new();
+        for line in src.lines() {
+            let reaction = Reaction::parse(line)?;
+            input.insert(reaction.to.clone(), ReactionInfo::new(reaction));
+        }
+
+        Ok(Self { input })
+    }
+
+    pub fn calculate(&mut self) -> usize {
+        // self.set_weight();
+
+        2
+    }
+
+    // fn set_weight() {}
+}
+
+fn main() -> Result<()> {
+    let input = fs::read_to_string("input.txt")?;
+    let lab = Lab::parse(&input)?;
+
+    dbg!(lab);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -51,9 +115,20 @@ mod test {
 
     #[test]
     fn test_parse_reaction() {
-        let reaction = Reaction::parse(" 10 ORE => 10 A \n").unwrap();
-        let expected = Reaction::new(10, "ORE", 10, "A");
+        let reaction = Reaction::parse(" 10 ORE, 1 C => 10 A \n").unwrap();
+        let expected = Reaction {
+            input: vec![(10, "ORE".to_owned()), (1, "C".to_owned())],
+            to: "A".to_owned(),
+            to_count: 10,
+        };
 
         assert_eq!(reaction, expected);
+    }
+
+    #[test]
+    fn test_lab_a() {
+        let mut lab = Lab::parse("1 ORE => 1 FUEL").unwrap();
+        let result = lab.calculate();
+        assert_eq!(1, result);
     }
 }
