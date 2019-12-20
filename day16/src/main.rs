1544d78e0e0e2167757cd0dbc953d89859393043
Eugene Korbut <me@mvl.ru>
2019-12-20 17:57:49 +0300

    16/1

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+
 const PATTERN: [i32; 4] = [0, 1, 0, -1];
 const PATTERN_SIZE: usize = 4;
 
@@ -39,8 +41,58 @@ impl Iterator for Pattern {
     }
 }
 
+#[derive(Debug, PartialEq)]
+struct Signal {
+    data: Vec<u8>,
+}
+
+impl Signal {
+    pub fn parse(input: &str) -> Self {
+        let data = input
+            .split("")
+            .filter_map(|ch| ch.parse::<u8>().ok())
+            .collect::<Vec<_>>();
+        Self { data }
+    }
+
+    pub fn len(&self) -> usize {
+        self.data.len()
+    }
+
+    pub fn next(mut self) -> Self {
+        let data = (0..self.len()).map(|n| {
+            self.data
+                .iter()
+                .zip(Pattern::new(n))
+                .map(|(a, b)| (*a as i32) * b)
+                .sum()
+        });
+
+        self.data = data.map(|u: i32| (u.abs() % 10) as u8).collect();
+        self
+    }
+
+    pub fn first_n(&self, n: usize) -> String {
+        self.data
+            .iter()
+            .take(n)
+            .fold(String::new(), |a, e| format!("{}{}", a, e))
+    }
+}
+
+fn task_a(input: &str) -> String {
+    let mut input = Signal::parse(&input);
+
+    for _ in 0..100 {
+        input = input.next();
+    }
+
+    input.first_n(8)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let raw = fs::read_to_string("input.txt").expect("cant read input.txt");
+    println!("task I: {}", task_a(&raw));
 }
 
 #[cfg(test)]
@@ -61,5 +113,28 @@ mod test {
         let expected = vec![0, 0, 0, 1, 1, 1];
 
         assert_eq!(values, expected);
+    }
+
+    #[test]
+    fn test_signal_parse() {
+        let input = "112\n";
+        let parsed = Signal::parse(&input);
+        let expected = Signal {
+            data: vec![1, 1, 2],
+        };
+
+        assert_eq!(expected, parsed);
+    }
+
+    #[test]
+    fn test_signal_next() {
+        assert_eq!(Signal::parse("12345678").next(), Signal::parse("48226158"));
+        assert_eq!(Signal::parse("34040438").next(), Signal::parse("03415518"));
+    }
+    #[test]
+    fn test_task_1() {
+        assert_eq!(task_a("80871224585914546619083218645595"), "24176176");
+        assert_eq!(task_a("19617804207202209144916044189917\n"), "73745418");
+        assert_eq!(task_a("69317163492948606335995924319873\n"), "52432133");
     }
 }
