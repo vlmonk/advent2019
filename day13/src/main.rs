516d11b76ea3746a77ea91ebb9ac223742e31a1b
Eugene Korbut <me@mvl.ru>
2020-01-27 20:39:57 +0300

    day 13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,4 +1,5 @@
 use crate::vm::{CPU, IO};
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt;
 use std::fs;
@@ -8,24 +9,28 @@ use std::thread;
 use std::time;
 use termion::{clear, color, cursor};
 
-enum Triplet<T>
-where
-    T: Copy,
-{
+enum Triplet {
     None,
-    One(T),
-    Two(T, T),
+    One(i64),
+    Two(i64, i64),
 }
 
-impl<T> Triplet<T>
-where
-    T: Copy,
-{
+fn convert(a: i64, b: i64, c: i64) -> Output {
+    if a == -1 && b == 0 {
+        Output::ScoreInfo(c)
+    } else {
+        let coord = Coord::new(a, b);
+        let tile = Tile::from_i64(c);
+        Output::BlockInfo(coord, tile)
+    }
+}
+
+impl Triplet {
     pub fn new() -> Self {
         Self::None
     }
 
-    pub fn add(&mut self, value: T) -> Option<(T, T, T)> {
+    pub fn add(&mut self, value: i64) -> Option<Output> {
         let result = match self {
             Self::Two(a, b) => Some((*a, *b, value)),
             Self::One(_) | Self::None => None,
@@ -38,11 +43,11 @@ where
         };
 
         mem::replace(self, replace);
-        result
+        result.map(|(a, b, c)| convert(a, b, c))
     }
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Copy, Clone)]
 enum Tile {
     Empty,
     Wall,
@@ -88,7 +93,7 @@ impl fmt::Display for Tile {
     }
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 struct Coord {
     x: i64,
     y: i64,
@@ -98,6 +103,11 @@ impl Coord {
     pub fn new(x: i64, y: i64) -> Self {
         Self { x, y }
     }
+}
+
+enum Output {
+    BlockInfo(Coord, Tile),
+    ScoreInfo(i64),
 }
 
 struct Game {
@@ -134,10 +144,8 @@ impl Game {
         let mut cpu = CPU::new_from_str(&self.input);
         cpu.set_mem(0, 2);
 
-        let draw = |x: i64, y: i64, c: i64| {
-            let goto = cursor::Goto(x as u16 + 1, y as u16 + 1);
-            let tile = Tile::from_i64(c);
-
+        let draw = |coord: Coord, tile| {
+            let goto = cursor::Goto(coord.x as u16 + 1, coord.y as u16 + 1);
             println!("{}{}", goto, tile);
         };
 
@@ -148,16 +156,19 @@ impl Game {
             println!("{}Score: {}", goto, v)
         };
 
-        let sleep_interval = time::Duration::from_millis(300);
+        let sleep_interval = time::Duration::from_millis(40);
 
         let input = || {
             thread::sleep(sleep_interval);
-            0
+            self.field.predict()
         };
 
         let output = |value| match triplet.add(value) {
-            Some((x, y, v)) if x == -1 && y == 0 => show_score(v),
-            Some((x, y, c)) => draw(x, y, c),
+            Some(Output::ScoreInfo(v)) => show_score(v),
+            Some(Output::BlockInfo(coord, tile)) => {
+                draw(coord, tile);
+                self.field.insert(coord, tile);
+            }
             None => {}
         };
 
@@ -170,22 +181,56 @@ impl Game {
 }
 
 struct Field {
-    data: HashMap<Coord, Tile>,
+    data: RefCell<HashMap<Coord, Tile>>,
+    ball: RefCell<Option<Coord>>,
+    paddle: RefCell<Option<Coord>>,
 }
 
 impl Field {
     pub fn new() -> Self {
         Self {
-            data: HashMap::new(),
+            data: RefCell::new(HashMap::new()),
+            ball: RefCell::new(None),
+            paddle: RefCell::new(None),
         }
     }
 
-    pub fn insert(&mut self, coord: Coord, tile: Tile) {
-        self.data.insert(coord, tile);
+    pub fn insert(&self, coord: Coord, tile: Tile) {
+        self.data.borrow_mut().insert(coord, tile);
+
+        if let Tile::Ball = tile {
+            self.ball.replace(Some(coord));
+        }
+
+        if let Tile::Paddle = tile {
+            self.paddle.replace(Some(coord));
+        }
     }
 
     pub fn block_num(&self) -> usize {
-        self.data.values().filter(|field| field.is_block()).count()
+        self.data
+            .borrow()
+            .values()
+            .filter(|field| field.is_block())
+            .count()
+    }
+
+    pub fn predict(&self) -> i64 {
+        let ball = *self.ball.borrow();
+        let paddle = *self.paddle.borrow();
+
+        match (ball, paddle) {
+            (Some(ball), Some(paddle)) => predict_by_coord(ball, paddle),
+            _ => 0,
+        }
+    }
+}
+
+fn predict_by_coord(ball: Coord, paddle: Coord) -> i64 {
+    match (ball.x, paddle.x) {
+        (ball, paddle) if ball > paddle => 1,
+        (ball, paddle) if ball < paddle => -1,
+        _ => 0,
     }
 }
 
