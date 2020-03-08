cd9cdbdd0b7289a68ec03f307c4122845013c807
Eugene Korbut <me@mvl.ru>
2020-03-09 00:06:58 +0300

    wip

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,5 +1,53 @@
 mod vm;
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+enum Item {
+    Scaffold,
+    Empty,
+    Robot(Direction),
+}
+
+trait Storage {
+    fn add(&mut self, item: Item);
+    fn width(&self) -> usize;
+    fn height(&self) -> usize;
+}
+
+struct Field {}
+
+
+impl Field {
+
+}
+
+'foo'
+
+impl Storage for Field {
+    fn new() -> Self {
+        Self {}
+    }
+    fn add(&mut self, item: Item) {}
+    fn width(&self) -> usize {
+        0
+    }
+    fn height(&self) -> usize {
+        0
+    }
+}
 
 fn main() {
     println!("hi man!!!!");
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_size_empty() {
+        let field = Field::new()
+    }
 }
